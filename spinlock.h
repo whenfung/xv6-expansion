3e4da8a3510526b51f9d79081f5f96730b7ba032
professordeng <2248262060@qq.com>
2020-02-15 14:16:37 +0800

    添加信号量数据结构

--- spinlock.h
+++ spinlock.h
@@ -10,3 +10,15 @@ struct spinlock {
 };
 
 int sh_var;          // 全局共享变量
+
+#define SEM_MAX_NUM 128          // 信号量总数
+struct sem{
+  struct spinlock lock;          // 内核自旋锁
+  int resource_count;            // 资源计数
+  int len;                       // 阻塞进程数量
+  int allocated;                 // 是否被分配：1 已分配，0 未分配
+  int procs[];                   // 阻塞进程号
+};
+
+extern int   sem_used_count;     // 当前在用信号量数目
+extern struct sem sems[SEM_MAX_NUM]; // 系统信号量上限
