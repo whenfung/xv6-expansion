d05f16bda133e985a972f31d7cbf1df78ead9503
professordeng <2248262060@qq.com>
2020-02-16 19:01:28 +0800

    完善测试样例

--- myalloc.c
+++ myalloc.c
@@ -4,6 +4,7 @@
 
 
 int main() {
+  
   char* m1 = (char*)myalloc(2 * 4096);
   char* m2 = (char*)myalloc(3 * 4096);
   char* m3 = (char*)myalloc(1 * 4096);
@@ -16,13 +17,14 @@ int main() {
 
   myfree(m2);
   
+//  尝试往空洞写数据  
 //  m2[0] = 'h';
 //  m2[1] = '\0';
 //  printf(1, "m1: %s\n", m2);
 
   myfree(m4);
 
-  sleep(10);
+  sleep(5000);
     
   myfree(m1);
   myfree(m3);
