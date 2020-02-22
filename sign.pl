59535c86d5254891dc05aa74d999909717194237
professordeng <2248262060@qq.com>
2020-02-22 22:41:23 +0800

    解读 bootblock 的生成

--- sign.pl
+++ sign.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/perl
 
-open(SIG, $ARGV[0]) || die "open $ARGV[0]: $!";
+open(SIG, $ARGV[0]) || die "open $ARGV[0]: $!"; # 打开命令行参数指出的文件
 
-$n = sysread(SIG, $buf, 1000);
+$n = sysread(SIG, $buf, 1000);  # 读入 1000 字节到 buf 中
 
-if($n > 510){
-  print STDERR "boot block too large: $n bytes (max 510)\n";
+if($n > 510){    # 如果读入字节数大于 510
+  print STDERR "boot block too large: $n bytes (max 510)\n"; # 大于盘块大小
   exit 1;
 }
 
-print STDERR "boot block is $n bytes (max 510)\n";
+print STDERR "boot block is $n bytes (max 510)\n"; # 提示启动扇区的有效字节数
 
 $buf .= "\0" x (510-$n);
-$buf .= "\x55\xAA";
+$buf .= "\x55\xAA";   # 启动盘块最后两个字节
 
 open(SIG, ">$ARGV[0]") || die "open >$ARGV[0]: $!";
-print SIG $buf;
+print SIG $buf;    # 将缓冲区内容写回文件
 close SIG;
