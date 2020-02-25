83ea50ece07348be307db8fff71e835f05ee91a9
professordeng <2248262060@qq.com>
2020-02-25 17:54:06 +0800

    将 tab 替换为 2 空格

--- sh_rw_lock.c
+++ sh_rw_lock.c
@@ -5,20 +5,20 @@
 int
 main()
 {
-	sh_var_write(0);
+  sh_var_write(0);
 
-  int id = sem_create(1);	
-	int pid = fork();
-		
-	for(int i = 0; i < 10000; i++) {
-		sem_p(id);
-		sh_var_write(sh_var_read()+1);
-		sem_v(id);
-	}
-	if(pid > 0){ 
-		wait();
-		sem_free(id);
-	}
-	printf(1,"sum = %d\n", sh_var_read());
-	exit();
+  int id = sem_create(1); 
+  int pid = fork();
+    
+  for(int i = 0; i < 10000; i++) {
+    sem_p(id);
+    sh_var_write(sh_var_read()+1);
+    sem_v(id);
+  }
+  if(pid > 0){ 
+    wait();
+    sem_free(id);
+  }
+  printf(1,"sum = %d\n", sh_var_read());
+  exit();
 }
