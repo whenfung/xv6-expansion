5218dcd43b5c6f5fa2a1e92caa3523cb8591d7a6
professordeng <2248262060@qq.com>
2020-02-25 12:11:43 +0800

    简化测试程序 loop

--- loop.c
+++ loop.c
@@ -3,17 +3,10 @@
 #include "user.h"
 
 int
-main(int argc, char *argv[])
+main()
 {
-  int pid;
-  int data[8];
-
-  pid = fork();
-  for(int i = 0; i < 2; i ++) {
-    for(int j = 0; j < 1024 * 100; j ++)
-      for(int k = 0; k < 1024 * 100; k ++)
-        data[k % 8] = pid * k;
-  }
-  printf(1, "%d", data[0]);
+  fork();
+  fork();
+  while(1); 
   exit();
 }
