9b632df1ef70778c4930f5e3b979734122d45454
professordeng <2248262060@qq.com>
2020-02-28 18:15:06 +0800

    注释

--- user.h
+++ user.h
@@ -2,26 +2,26 @@ struct stat;
 struct rtcdate;
 
 // system calls
-int fork(void);
-int exit(void) __attribute__((noreturn));
-int wait(void);
-int pipe(int*);
-int write(int, const void*, int);
-int read(int, void*, int);
-int close(int);
-int kill(int);
-int exec(char*, char**);
-int open(const char*, int);
-int mknod(const char*, short, short);
-int unlink(const char*);
-int fstat(int fd, struct stat*);
-int link(const char*, const char*);
-int mkdir(const char*);
-int chdir(const char*);
-int dup(int);
-int getpid(void);
-char* sbrk(int);
-int sleep(int);
+int fork(void);  // 创建进程
+int exit(void) __attribute__((noreturn));  // 结束当前进程
+int wait(void);   // 等待子进程结束
+int pipe(int*);   // 创建管道，并把读和写的 fd 返回到 p
+int write(int, const void*, int);  // 从 buf 中写 n 个字节到文件
+int read(int, void*, int);  // 从文件中读 n 个字节到 buf
+int close(int);    // 关闭打开的 fd
+int kill(int);     // 结束 pid 所指进程
+int exec(char*, char**);  // 加载并执行一个文件
+int open(const char*, int);  // 打开文件，flags 指定读/写模式
+int mknod(const char*, short, short); // 创建设备文件
+int unlink(const char*);   // 删除文件
+int fstat(int fd, struct stat*);  // 返回文件信息
+int link(const char*, const char*); // 给 f1 创建一个新名字 f2
+int mkdir(const char*); // 创建新的目录
+int chdir(const char*);   // 改变当前目录
+int dup(int);     // 复制 fd
+int getpid(void);  // 获得当前进程 pid
+char* sbrk(int);   // 为进程内存空间增加 n 字节
+int sleep(int);    // 睡眠 n 秒
 int uptime(void);
 
 // ulib.c
