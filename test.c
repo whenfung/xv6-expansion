e7d1a3f32e2a8c0f921a54a3419ca14ce93438b1
professordeng <2248262060@qq.com>
2020-03-17 20:31:09 +0800

    简化测试样例

--- test.c
+++ test.c
@@ -4,41 +4,38 @@
 
 void
 mem(void) {
-	printf(1, "分页式开始测试\n");
+	printf(1, "请求调页功能开始测试\n");
   
-  // 测试只分配不写时剩余物理页帧的变化 
-  char *start =(char*) malloc(4096*5);
-
-  // 测试分配且写时剩余物理页帧的变化，并查看其线性地址是否连续。
-	start = (char*)malloc(4096);       
+  // 分配内存并写内存
+	char* start = (char*)malloc(4096);     // 分配 4K
 	char* pre = start;
 
-	for(int i = 1; i <= 10; i++) {
+	for(int i = 1; i <= 10; i++) {   // 分配 10 次 4K
 		char* nex = (char*)malloc(4096);
 		if (nex == 0)
 			goto failed;
-		*(char**)pre = nex;
-		((int*)pre)[2] = i;
+		*(char**)pre = nex;            // 将内存段串成链表
+		((int*)pre)[2] = i;            // 分配顺序
 		pre= nex;
 	}
 
-	((int*)pre)[2] = 11;   // 第 11 块
+	((int*)pre)[2] = 11;             // 给第 11 块内存段标号
 
+  // 输出相关信息
   while(1){
     printf(1, "%d : %p \n", ((int*)start)[2], (char*)start);
     if(((int*)start)[2] == 11) break;
     start = *(char**)start;
   }
-	printf(1, "请求分页机制测试成功！\n");
+	printf(1, "请求调页机制测试成功！\n");
 	return;
 failed:
-	printf(1, "请求分页机制测试失败！\n");
+	printf(1, "请求调页机制测试失败！\n");
 }
 
 int
 main()
 {
 	mem();
-	printf(1, "退出\n");
   exit();
 }
