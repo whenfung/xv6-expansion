ca11f05e96cb9b35f40173544a835a26d11aa657
professordeng <2248262060@qq.com>
2020-03-23 16:58:05 +0800

    添加测试样例

---
+++ test.c
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void 
+worker(void *arg) {
+  *(int*)arg = 2; 
+  exit();
+}
+
+int
+main()
+{
+  void* stack = malloc(4096*2);
+  if((uint)stack % 4096)
+    stack += (4096 - (uint)stack % 4096);
+
+  int arg = 1;
+  clone(worker, (void*)&arg, stack);
+  exit();
+}
+
+
