ce8c3dd0cbfab9215976584e5de4a28db6c1ff13
professordeng <2248262060@qq.com>
2020-03-25 23:17:26 +0800

    改名

--- thread.c
+++ thread.c
@@ -2,6 +2,11 @@
 #include "stat.h"
 #include "user.h"
 
+#define PGSIZE 4096
+
+int thread_create(void (*fcn)(void*), void *arg);
+int thread_join();
+
 void 
 worker(void *arg) {
   *(int*)arg = 2; 
@@ -11,14 +16,22 @@ worker(void *arg) {
 int
 main()
 {
-  void* stack = malloc(4096*2);
-  if((uint)stack % 4096)
-    stack += (4096 - (uint)stack % 4096);
-
   int arg = 1;
-  clone(worker, (void*)&arg, stack);
-  join(&stack); 
+  thread_create(worker, (void*)&arg);
+  thread_join();
   exit();
 }
 
+int thread_create(void (*fcn)(void*), void *arg) {
+  void *stack = malloc(2 * PGSIZE);
+  if((uint)stack % PGSIZE)
+    stack = stack + (PGSIZE - (uint)stack % PGSIZE);
+  return clone(fcn, arg, stack); 
+}
 
+int thread_join() {
+  void *stack;
+  int ret = join(&stack);
+  // free(stack);
+  return ret;
+}
