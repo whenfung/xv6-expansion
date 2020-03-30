65960b25482649aa4f3d2b2ada9b6daacc2bd3da
professordeng <2248262060@qq.com>
2020-03-30 15:29:15 +0800

    线程号改名

--- uthread.c
+++ uthread.c
@@ -5,16 +5,16 @@
 #define PGSIZE  4096
 
 struct {
-  int pid;
+  int tid;
   void* ustack;
   int used;
 } threads[NTHREAD];   // TCB 表
 
 // add a TCB to thread table
-void add_thread(int* pid, void* ustack) {
+void add_thread(int* tid, void* ustack) {
   for(int i = 0; i < NTHREAD; i++) {
     if(threads[i].used == 0) {
-      threads[i].pid = *pid;
+      threads[i].tid = *tid;
       threads[i].ustack = ustack;
       threads[i].used = 1;
       break;
@@ -22,11 +22,11 @@ void add_thread(int* pid, void* ustack) {
   }
 }
 
-void remove_thread(int* pid) {
+void remove_thread(int* tid) {
   for(int i = 0; i < NTHREAD; i ++) {
-    if(threads[i].used && threads[i].pid == *pid) {
+    if(threads[i].used && threads[i].tid == *tid) {
       free(threads[i].ustack);   // 释放用户栈
-      threads[i].pid = 0;
+      threads[i].tid = 0;
       threads[i].ustack = 0;
       threads[i].used = 0;
       break;
@@ -40,26 +40,26 @@ int thread_create(void (*start_routine)(void*), void* arg) {
   if(first) {
     first = 0;
     for(int i = 0; i < NTHREAD; i++) {
-      threads[i].pid = 0;
+      threads[i].tid = 0;
       threads[i].ustack = 0;
       threads[i].used = 0;
     }
   }
   void* stack = malloc(PGSIZE);   // allocate one page for user stack
-  int pid = clone(start_routine, arg, stack); // system call for kernel thread
-  add_thread(&pid, stack);  // save new thread to thread table
+  int tid = clone(start_routine, arg, stack); // system call for kernel thread
+  add_thread(&tid, stack);  // save new thread to thread table
 //  printf(1, "arg addr= %x\n", (int)(stack + 4096 - 4));
 //  printf(1, "PC = %x\n", *(int*)(stack + 4096 - 8));
-  return pid;
+  return tid;
 }
 
 int thread_join(void) {
   for(int i = 0; i < NTHREAD; i ++) {
     if(threads[i].used == 1) {
-      int pid = join();      // 回收子线程
-      if(pid > 0) {
-        remove_thread(&pid);
-        return pid;
+      int tid = join();      // 回收子线程
+      if(tid > 0) {
+        remove_thread(&tid);
+        return tid;
       }
     }
   }
