75f54d66899bdab93c7fb6f12d43742cca6220a5
professordeng <2248262060@qq.com>
2020-05-01 10:53:47 +0800

    多线程时间测试

--- test2.c
+++ test2.c
@@ -4,7 +4,7 @@
 void worker(void* arg) {
   // 线程工作
   float x = 1.0;
-  for(float i = 0; i < 120; i+=1) 
+  for(float i = 0; i < 15; i+=1) 
     for(float j = 0; j < 100000; j+=0.1) {
       x = x*1*3.14*2.18*1.99;
       x = 1.0;
@@ -18,10 +18,13 @@ main()
 {
   uint start = uptime();
   int t1=1;
+  int t2=2;
   thread_create(worker, &t1);
+  thread_create(worker, &t2);
+  thread_join();
   thread_join();
   uint end = uptime();
-  printf(1, "%d\n", end-start);
+  printf(1, "花费时间：%d\n", end-start);
 
   exit();
 }
--- test3.c
+++ test3.c
@@ -4,7 +4,7 @@
 void worker(void* arg) {
   // 线程工作
   float x = 1.0;
-  for(float i = 0; i < 40; i+=1) 
+  for(float i = 0; i < 10; i+=1) 
     for(float j = 0; j < 100000; j+=0.1) {
       x = x*1*3.14*2.18*1.99;
       x = 1.0;
@@ -27,7 +27,7 @@ main()
   thread_join();
   thread_join();
   uint end = uptime();
-  printf(1, "%d\n", end-start);
+  printf(1, "花费时间：%d\n", end-start);
 
   exit();
 }
