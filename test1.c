a579e2c06ba7c21f8f64dbfaad51af424604ed48
professordeng <2248262060@qq.com>
2020-05-12 00:49:43 +0800

    浮点数不显示问题

--- test1.c
+++ test1.c
@@ -1,29 +1,11 @@
 #include "types.h"
 #include "user.h"
 
-void worker(void* arg) {
-  // 线程工作
-  uint start = uptime();
-  float x = 1.0;
-  for(float i = 0; i < 30; i+=1) 
-    for(float j = 0; j < 100000; j+=0.1) {
-      x = x*1*3.14*2.18*1.99;
-      x = 1.0;
-    }
-  uint end = uptime();
-  printf(1, "%d 线程完成工作 %d\n", *(int*)arg, end - start);
-  exit();
-}
-
 int
 main()
 {
-  uint start = uptime();
-  int t1=1;
-  thread_create(worker, &t1);
-  thread_join();
-  uint end = uptime();
-  printf(1, "花费时间：%d\n", end-start);
+  float x = 6.1;
+  printf(1, "x = %d\n", (int)x);
 
   exit();
 }
