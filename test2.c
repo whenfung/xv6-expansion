094d1df728699cc7efcb3e06a84c98cba1efa850
professordeng <2248262060@qq.com>
2020-05-12 11:44:18 +0800

    开销测试

--- test2.c
+++ test2.c
@@ -1,32 +1,26 @@
 #include "types.h"
 #include "user.h"
 
-void worker(void* arg) {
-  uint start = uptime();
-  
-  // 线程工作
-  float x = 1.0;
-  for(float i = 0; i < 15; i+=1) 
-    for(float j = 0; j < 100000; j+=0.1) {
-      x = x*1*3.14*2.18*1.99;
-      x = 1.0;
-    }
+int n;
 
-  uint end = uptime();
-  printf(1, "%d 线程完成工作 %d\n", *(int*)arg, end - start);
+void worker(void* arg) {
   exit();
 }
 
-int
-main()
+int main(int argc, char **argv)
 {
+  if (argc != 2) exit();
+  
+  n = atoi(argv[1]);
+  printf(1, "执行 %d 个线程切换\n", n);
+
   uint start = uptime();
-  int t1=1;
-  int t2=2;
-  thread_create(worker, &t1);
-  thread_create(worker, &t2);
-  thread_join();
-  thread_join();
+  
+  for(int i = 0; i < n; i++) {
+    thread_create(worker, &i);
+    thread_join();
+  }
+
   uint end = uptime();
   printf(1, "花费时间：%d\n", end-start);
 
